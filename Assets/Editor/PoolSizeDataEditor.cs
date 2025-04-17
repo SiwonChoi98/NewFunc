49e2d679aaa75fd4b99ce0cd98fe67dbd938ed19
bestvalue0410@naver.com <bestvalue0410@naver.com>
2025-04-17 16:38:12 +0900

    [fix] undo 가능한 형태로 수정

--- Assets/Editor/PoolSizeDataEditor.cs
+++ Assets/Editor/PoolSizeDataEditor.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 [CustomEditor(typeof(PoolSizeData))]
 public class PoolSizeDataEditor : Editor
 {
+    /*
     // 인스펙터 UI를 커스텀할 때 사용하는 메서드
     public override void OnInspectorGUI()
     {
@@ -74,5 +75,79 @@ public class PoolSizeDataEditor : Editor
 
         serializedObject.ApplyModifiedProperties(); // 변경 사항 적용
         EditorUtility.SetDirty(data); // 데이터가 수정되었음을 Unity에 알림 (저장을 위해)
+    }*/
+    
+    private SerializedProperty poolSettingsProperty;
+
+    private void OnEnable()
+    {
+        // poolSettings 리스트를 SerializedProperty로 가져옴
+        poolSettingsProperty = serializedObject.FindProperty("poolSettings");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update(); // 최신 상태로 갱신
+
+        EditorGUILayout.HelpBox("이 설정은 사이즈 조절이 필요한 오브젝트(PoolObjectType)에만 사용하세요.", MessageType.Warning);
+        EditorGUILayout.LabelField("Pool Size Settings", EditorStyles.boldLabel);
+
+        for (int i = 0; i < poolSettingsProperty.arraySize; i++)
+        {
+            SerializedProperty settingProp = poolSettingsProperty.GetArrayElementAtIndex(i);
+            SerializedProperty typeProp = settingProp.FindPropertyRelative("poolType");
+            SerializedProperty sizeProp = settingProp.FindPropertyRelative("maxSize");
+
+            EditorGUILayout.BeginVertical("box");
+
+            // 풀 타입 선택 (Enum)
+            EditorGUILayout.PropertyField(typeProp, new GUIContent("Pool Type"));
+
+            // 최대 크기 슬라이더
+            sizeProp.intValue = EditorGUILayout.IntSlider("Max Size", sizeProp.intValue, 1, 200);
+
+            // 중복 검사
+            bool isDuplicate = false;
+            for (int j = 0; j < poolSettingsProperty.arraySize; j++)
+            {
+                if (j == i) continue;
+                var other = poolSettingsProperty.GetArrayElementAtIndex(j);
+                var otherType = other.FindPropertyRelative("poolType");
+
+                if (otherType.enumValueIndex == typeProp.enumValueIndex)
+                {
+                    isDuplicate = true;
+                    break;
+                }
+            }
+
+            if (isDuplicate)
+            {
+                EditorGUILayout.HelpBox("이 Pool Type은 이미 존재합니다!", MessageType.Warning);
+            }
+
+            GUI.backgroundColor = Color.red;
+            if (GUILayout.Button("Remove"))
+            {
+                poolSettingsProperty.DeleteArrayElementAtIndex(i);
+                break; // 리스트 수정했으니 루프 중단
+            }
+            GUI.backgroundColor = Color.white;
+
+            EditorGUILayout.EndVertical();
+            EditorGUILayout.Space();
+        }
+
+        GUI.backgroundColor = Color.green;
+        if (GUILayout.Button("Add Pool Type"))
+        {
+            poolSettingsProperty.InsertArrayElementAtIndex(poolSettingsProperty.arraySize);
+            var newElement = poolSettingsProperty.GetArrayElementAtIndex(poolSettingsProperty.arraySize - 1);
+            newElement.FindPropertyRelative("poolType").enumValueIndex = 0;
+            newElement.FindPropertyRelative("maxSize").intValue = 10; // 기본값 설정
+        }
+        GUI.backgroundColor = Color.white;
+
+        serializedObject.ApplyModifiedProperties(); // 변경사항 반영
     }
 }
